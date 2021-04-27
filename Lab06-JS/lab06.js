20dc248f2f7c1b496a6f3048ccd6015f11588319
Wondimu <wondimu.kenea@aims-cameroon.org>
2021-04-27 16:24:01 -0500

    Lab 6 added

--- Lab06-JS/lab06.js
+++ Lab06-JS/lab06.js
@@ -1,9 +1,51 @@
+// Question 1: Code fix
+function makeArmy() {
+    let shooters = [];
+    let i = 0;
+    while (i < 2) {
+        let j = i;
+        let shooter = function() {
+            console.log(j);
+        };
+        shooters.push(shooter);
+        i++;
+    }
+    return shooters;
+}
+let army = makeArmy();
+army[0]();
+army[1]();
+
+
+// Question two 
 function printNumbers(from,to){
-setInterval(()=>{
+let timerID= setInterval(()=>{
     if(from<=to){
         console.log(from);
+        if(from==to){
+            clearInterval(timerID);
+        }
         from++;
     }
-},1000)};
+},3000)};
 printNumbers(5,7);
+
+// Question 3 - explanation
+// let i=0;
+// setTimeout(()=>alert(i),100);
+// for(let j=0;j<100000000;j++){
+//     i++;
+// }
+// // The scheduled function run once current code finished. Because setTimeout simply queues the code to run once the current call stack is finished executing.
+// // Once done, the setTimeout will execute the alert which will be popout window with the value of i (i.e. 100000000).
+
+let i=0;
+setTimeout(()=>alert(i),100);
+
+setTimeout(()=>{
+    for(let j=0;j<100000000;j++){
+    i++;
+    }
+},1000)
+
 
